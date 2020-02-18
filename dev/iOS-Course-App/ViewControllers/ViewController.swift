29714dd19f3734a978539e1fc6a727c93b6bc994
Violeta Ilieva <vilettt@abv.bg>
2020-02-18 16:08:21 +0200

    add ui changes

---
+++ dev/iOS-Course-App/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Course-App
+//
+//  Created by TsvetoslavVasev on 16.02.20.
+//  Copyright © 2020 TsvetoslavVasev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
