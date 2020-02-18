da229793abb19a089b185d4cddf6676c22836f5f
Violeta Ilieva <vilettt@abv.bg>
2020-02-18 14:12:37 +0200

    adding local ui version

---
+++ Violeta development/IOSCourceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSCourceApp
+//
+//  Created by Violeta Ilieva on 6.02.20.
+//  Copyright © 2020 FMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
