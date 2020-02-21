0afcc1bb95a9904c2aa7ab1e81f64238ef1e99d4
Demetra Staykova <demschoy@gmail.com>
2020-02-21 14:48:39 +0200

    added youtube and links working version of player. yet not supporting google drive videos

--- iOS-Course-App/iOS-Course-App/ViewControllers/ProjectViewController.swift
+++ iOS-Course-App/iOS-Course-App/ViewControllers/ProjectViewController.swift
@@ -5,7 +5,7 @@
 //  Created by DemetraStaykova on 19.02.20.
 //  Copyright © 2020 TsvetoslavVasev. All rights reserved.
 //
-
+import WebKit
 import UIKit
 import AVKit
 import AVFoundation
@@ -17,7 +17,8 @@ class ProjectViewController: UIViewController {
     @IBOutlet weak var projectNameLabel: UILabel!
     @IBOutlet weak var projectDescriptionLabel: UILabel!
     @IBOutlet weak var gitRepositoryButton: UIButton!
-   
+    @IBOutlet weak var webView: WKWebView!
+    
     var projectInfo: Project?
     var player = AVPlayer()
     override func viewDidLoad() {
@@ -49,14 +50,37 @@ class ProjectViewController: UIViewController {
     }
     
     @IBAction func playButtonTapped(_ sender: Any) {
-        let videoUrl = NSURL(string: projectInfo?.videoUrl ?? "https://devstreaming-cdn.apple.com/videos/app_store/app-store-product-page/hls_vod_mvp.m3u8")!
-        player = AVPlayer(url: videoUrl as URL)
+        var videoUrl = URL(string: "")
+        if let url = projectInfo?.videoUrl {
+            if url != "" {
+                videoUrl = NSURL(string: url)! as URL
+            }
+            else {
+               videoUrl = NSURL(string: "https://www.youtube.com/watch?v=nyp_PczrqFE")! as URL
+            }
+        } else {
+           videoUrl = NSURL(string: "https://devstreaming-cdn.apple.com/videos/app_store/app-store-product-page/hls_vod_mvp.m3u8")! as URL
+        }
+        
+        webView.configuration.allowsInlineMediaPlayback = true
+        webView.configuration.mediaTypesRequiringUserActionForPlayback = []
+        let request = URLRequest(url: videoUrl!)
+        webView.load(request)
+  
+    /* alternative player, not supportin youtube
+         
+        let videoUrl = NSURL(string: projectInfo?.videoUrl ?? "https://devstreaming-cdn.apple.com/videos/app_store/app-store-product-page/hls_vod_mvp.m3u8")! as URL
+        player = AVPlayer(url: videoUrl)
         let playerController = AVPlayerViewController()
         playerController.player = player
         self.showDetailViewController(playerController, sender: nil)
       playerController.view.frame = self.view.frame
         self.view.addSubview(playerController.view)
         player.play()
+*/
     }
 
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
